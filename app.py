ea2fe6d509cca35dfbd63f68aed745861ad43fe1
zeel249 <zeel@arkayapps.com>
2020-06-15 18:07:34 +0530

    [Fixed]Do not include local user specific values.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] ='localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Zeel@123'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'user'
 mysql = MySQL(app)
 bcrypt = Bcrypt()
